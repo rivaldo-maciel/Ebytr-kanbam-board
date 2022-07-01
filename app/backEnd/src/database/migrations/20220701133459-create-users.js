711773d8a8b3f6cd6574f9d9d159e8f021aba416
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-07-01 10:50:32 -0300

    feat(user migration): crate user migration
    crate user migration for user table creation

---
+++ app/backEnd/src/database/migrations/20220701133459-create-users.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'first_name'
+      },
+      last_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'last_name'
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      profileImage: {
+        type: Sequelize.STRING
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
