3de822d3043e533ad944608cc0e4a3e645f1a598
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-07-04 17:01:01 -0300

    feat(login controllers): create login controllers whit jwt authentication

---
+++ app/backEnd/src/controllers/Login.controllers.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from 'express';
+import ILogin from '../services/interfaces/Login.interface';
+import ILoginControllers from './interfaces/Login.interface';
+import * as jwt from 'jsonwebtoken';
+import { SignOptions } from 'jsonwebtoken';
+import 'dotent/config';
+
+class LoginControllers implements ILoginControllers {
+  private _loginServices: ILogin;
+
+  constructor(services: ILogin) {
+    this._loginServices = services;
+  }
+
+  public async login(req: Request, res: Response, next: NextFunction): Promise<void | Response> {
+    try {
+      const { email, password } = req.body;
+      await this._loginServices.login(email, password);
+      const secret = process.env.JWT_SECRET;
+      const jwtConfig = {
+        expiresIn: '1d',
+        algorithm: 'HS256',
+      };
+      const token = jwt.sign({ email, password }, secret as string, jwtConfig as SignOptions);
+      return res.status(200).json({ token });
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+export default LoginControllers;
